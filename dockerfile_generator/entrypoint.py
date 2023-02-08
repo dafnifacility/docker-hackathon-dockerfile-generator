2668ede633c81ecac58e54c13a9051cc4e08c000
Alex Manning <alexander.manning@stfc.ac.uk>
2023-02-08 14:20:01 +0000

    Write out rendered template.

--- dockerfile_generator/entrypoint.py
+++ dockerfile_generator/entrypoint.py
@@ -7,18 +7,17 @@ def main():
     ## Call script to clone git repo.
 
     ## Get the configuration
-    parsed = parser.parse_full("/data/source")
+    config = parser.parse_full("/data/source")
 
     ## Choose correct dockerfile template.
     template_env = jinja2.Environment(
         loader=jinja2.PackageLoader("dockerfile_generator", "templates"),
         autoescape=False,
     )
-    template = template_env.get_template("python.jinja")
+    template = template_env.get_template(f"{config['language']}.jinja")
 
     # Render template with vars and write out.
-    vars_ = {"foo": "hello"}
-    print(template.render(**vars_))
+    template.stream(**config).dump("/data/Dockerfile")
 
 
 if __name__ == "__main__":
